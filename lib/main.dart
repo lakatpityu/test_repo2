1b810df40629fb0877737b15a22435708c2d9112
root <root@vmi348945.contaboserver.net>
2020-09-16 13:45:21 +0200

    test snap 3

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your hello app.
+  // This widget is the root of your he
+  //llo app.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
